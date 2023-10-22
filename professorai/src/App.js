a305920e46d4d44381eed82708e400c9de1b91f5
khagendra01 <khagendrakhatri365@gmail.com>
2023-10-22 18:57:35 -0400

    soome cahnges

--- professorai/src/App.js
+++ professorai/src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
-
-const Home = () => <div>Home Page</div>;
-const OtherPage = () => <div>Other Page</div>;
 
 function App() {
   return (
-    <Router>
-      <Route path="/" exact component={Home} />
-      <Route path="/other" exact component={OtherPage} />
-      <Redirect from="*" to="/" />
-    </Router>
-  );
+    <>
+    <h1> Hello WOrld</h1>
+    <input> </input>
+    </>
+  )
 }
 
 export default App;
