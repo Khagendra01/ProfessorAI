d5d26003b44ae76e7e03ab44ed26c193472d81b7
Kgen <kgen@macbook.mynetworksettings.com>
2023-10-26 10:54:36 -0400

    changes

--- professorai/src/App.js
+++ professorai/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignInPrompt from './login';
 import MainPage from './mainPage';
 import ChatApp from './chatPlace';
+import Quiz from './quiz';
+import Feedback from './feedback';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<SignInPrompt />} />
         <Route path="/mainPage" element={<MainPage />} />
         <Route path="/chatPlace" element={<ChatApp />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/feedback" element={<Feedback />} />
       </Routes>
     </BrowserRouter>
   );
--- professorai/src/mainPage.js
+++ professorai/src/mainPage.js
@@ -4,19 +4,26 @@ import { useNavigate } from "react-router-dom";
 
 function MainPage() {
   const navigate = useNavigate();
-  const goToChat = () => {
-    navigate("/chatPlace");
+  const goTo = ( x ) => {
+    if(x === 1)
+    {
+      navigate("/chatPlace");
+    }else if(x === 2)
+    {
+      navigate("/quiz");
+    }
+    navigate("/feedback");
   };
 
   return (
     <div className="centered-buttons">
-      <button onClick={goToChat} className="big-button">
+      <button onClick={goTo(1)} className="big-button">
         Ask the AI
       </button>
       <br></br>
-      <button className="big-button">Practice the quiz</button>
+      <button onClick={goTo(2)} className="big-button">Practice the quiz</button>
       <br></br>
-      <button className="big-button">Get your feedback</button>
+      <button onClick={goTo(3)} className="big-button">Get your feedback</button>
       <br></br>
     </div>
   );
