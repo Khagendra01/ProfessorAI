d49a4dfa1af705ff56d911912dcd263b84602955
khagendra01 <khagendrakhatri365@gmail.com>
2023-11-06 13:48:52 -0500

    app plan up

--- professorai/src/views/mainPage.js
+++ professorai/src/views/mainPage.js
@@ -26,7 +26,19 @@ function MainPage() {
         <h1 className="main-title">Hey {user.firstName}, Welcome to Our App</h1>
         <div className="button-container">
           <button onClick={() => handleNavigation("/chatPlace")} className="big-button">
-            Ask the AI
+            Talk to my AI
+          </button>
+          <button onClick={() => handleNavigation("/Take a Note")} className="big-button">
+            Take a Note
+          </button>
+          <button onClick={() => handleNavigation("/")} className="big-button">
+            Learn Something New
+          </button>
+          <button onClick={() => handleNavigation("/")} className="big-button">
+            Improve self
+          </button>
+          <button onClick={() => handleNavigation("/")} className="big-button">
+            Prepare for the exam
           </button>
           <button onClick={() => handleNavigation("/quiz")} className="big-button">
             Practice the Quiz
