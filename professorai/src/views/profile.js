df5138a5ec5a999a8bf2daf1fb3abe9d40099f00
khagendra01 <khagendrakhatri365@gmail.com>
2023-11-13 15:07:42 -0500

    profile update

--- professorai/src/views/profile.js
+++ professorai/src/views/profile.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import '../styles/profile.css';
 import Navbar from '../components/navbar';
 import { AuthContext } from '../App';
@@ -9,9 +9,29 @@ import { AuthContext } from '../App';
 function Profile() {
   const { user } = useContext(AuthContext);
   const userProfile = {
-    username: user.firstName + " " + user.lastName ,
+    username: user.lastName ,
     points: 500,
     level: 5,
+  };
+
+  const [subjects, setSubjects] = useState(['Math', 'Science', 'History']);
+
+  
+  const [newSubject, setNewSubject] = useState('');
+
+  
+  const addSubject = () => {
+    if (newSubject.trim() !== '') {
+      setSubjects([...subjects, newSubject]);
+      setNewSubject('');
+    }
+  };
+
+  
+  const removeSubject = (index) => {
+    const updatedSubjects = [...subjects];
+    updatedSubjects.splice(index, 1);
+    setSubjects(updatedSubjects);
   };
 
   return (
@@ -22,18 +42,39 @@ function Profile() {
       <div className="profile-header">
         <h1>Welcome, {userProfile.username}</h1>
       </div>
-      <div className="profile-dashboard">
-        <div className="dashboard-item">
-          <h2>Points</h2>
-          <p>{userProfile.points}</p>
-        </div>
-        <div className="dashboard-item">
-          <h2>Level</h2>
-          <p>{userProfile.level}</p>
-        </div>
+    </div>
+      <div>
+      <h2>Student Profile</h2>
+      
+      {/* Display list of subjects */}
+      <div>
+        <h3>Subjects Taken:</h3>
+        <ul>
+          {subjects.map((subject, index) => (
+            <li key={index}>
+              {subject}
+              <button onClick={() => removeSubject(index)}>Remove</button>
+            </li>
+          ))}
+        </ul>
       </div>
+
+      {/* Add a new subject */}
+      <div>
+        <h3>Add a Subject:</h3>
+        <input
+          type="text"
+          value={newSubject}
+          onChange={(e) => setNewSubject(e.target.value)}
+        />
+        <button onClick={addSubject}>Add Subject</button>
+      </div>
+
+      {/* Other features can be added here based on your requirements */}
     </div>
+
     </div>
+    
     </>
   );
 }
