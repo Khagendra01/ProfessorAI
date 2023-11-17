40eaaf14085f218f2cd03381d3f1f7e17101dfdd
khagendra01 <khagendrakhatri365@gmail.com>
2023-11-17 11:24:23 -0500

    Try added for try

---
+++ professorai/src/views/try.js
@@ -0,0 +1,16 @@
+// App.js
+import React from 'react';
+import './styles/Try.css';
+
+const Try = () => {
+  return (
+    <div className="button-container">
+    <button className="talk-button">Talk</button>
+    <button className="sing-button">Sing</button>
+    <button className="dance-button">Dance</button>
+    <button className="laugh-button">Laugh</button>
+  </div>
+  );
+};
+
+export default Try;
