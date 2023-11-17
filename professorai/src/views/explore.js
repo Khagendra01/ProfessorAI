1903a61a514c865a4b6afa947ee501dd09f9ff3e
khagendra01 <khagendrakhatri365@gmail.com>
2023-11-17 11:15:33 -0500

    explore added

---
+++ professorai/src/views/explore.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/navbar';
+
+function Explore(props) {
+    return (
+        <div className='explore-container'>
+           <Navbar />
+
+
+        </div>
+    );
+}
+
+export default Explore;
