13178cbe7943ea638ef29c101dbaa673db67a808
khagendra01 <khagendrakhatri365@gmail.com>
2023-11-20 10:36:15 -0500

    patches

--- professorai/src/components/quiz.js
+++ professorai/src/components/quiz.js
@@ -4,33 +4,30 @@ import "./styles/Quiz.css"; // Import the CSS file for styling
 import { sendQuiz } from "../api/chatApi";
 
 function Quiz() {
-  const [questions, setQuestions] = useState([{id: 0, questionText: "Question", options: ["a", "b", "c", "d"], correctAnswer: "Amswer"}]);
+  const [questions, setQuestions] = useState([{id: 0, questionText: "Question", options: ["a", "b", "c", "d"], correctAnswer: "Answer"}]);
 
-  const [loading, setLoading] = useState(true);
-
-  var quizQuerry = [ {role: "user", content: "Generate 10 multiple-choice questions on the topic of calculus with 4 options and indicate the correct answer." }]
-
-
-  useEffect(() => {
-    const fetchQuestions = async () => {
-      try {
-        const response = await sendQuiz(quizQuerry);
-        setQuestions(response);
-        console.log(response);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error", error);
-      }
-    };
-  
-    fetchQuestions(); // Fetch questions when the component mounts
-  }, [loading]); 
-
+  const [loading, setLoading] = useState(false);
 
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [score, setScore] = useState(0);
   const [showResult, setShowResult] = useState(false);
 
+  const [balancer, setBalancer] = useState(true);
+
+  var quizQuerry = [ {role: "user", content: "Generate 10 multiple-choice questions on the topic of calculus with 4 options and indicate the correct answer. Give me in the format \"1. Question\na) Option1\nb) Option2\nc) Option3\nd) Option4\nAnswer: a) Option1.\n\n\"" }]
+
+
+
+  const fetchQuestions = async () => {
+    try {
+      const response = await sendQuiz(quizQuerry);
+      setQuestions(response);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error", error);
+
+    }
+  };
   const handleAnswerClick = (selectedAnswer) => {
     if (selectedAnswer === questions[currentQuestion].correctAnswer) {
       setScore(score + 1);
@@ -46,13 +43,13 @@ function Quiz() {
   return (
       <div className="quiz-container">
         { loading && <p>loading...</p> }
-
+        { !loading && balancer && <button onClick={() => {fetchQuestions(); setLoading(true); setBalancer(false)}}>start</button> }
         {showResult ? (
           <div className="quiz-result">
             <h2>Quiz Results</h2>
             <p>{`You scored ${score} out of ${questions.length}.`}</p>
           </div>
-        ) :   !loading && ( 
+        ) :   !loading && !balancer &&( 
           <div className="quiz-question">
             <h2>{`Question ${currentQuestion + 1}`}</h2>
             <p>{questions[currentQuestion].questionText}</p>
