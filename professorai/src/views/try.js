695bdbc12198dc76b9982fe944636b08ddde8e1d
khagendra01 <khagendrakhatri365@gmail.com>
2023-11-20 10:33:58 -0500

    just function tried

--- professorai/src/views/try.js
+++ professorai/src/views/try.js
@@ -1,15 +1,20 @@
-// App.js
-import React from 'react';
-import './styles/Try.css';
+
+import React, { useEffect, useState } from 'react';
+
 
 const Try = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+   
+    setCount(prevCount => prevCount + 1);
+    console.log("apple")
+  }, []); 
+
   return (
-    <div className="button-container">
-    <button className="talk-button">Talk</button>
-    <button className="sing-button">Sing</button>
-    <button className="dance-button">Dance</button>
-    <button className="laugh-button">Laugh</button>
-  </div>
+    <div className="try-container">
+      <h1>Count: {count}</h1>  
+    </div>
   );
 };
 
