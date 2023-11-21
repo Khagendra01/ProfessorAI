aa8db81b4cfd32a4022ed8fcb169f2bf2cae35da
khagendra01 <khagendrakhatri365@gmail.com>
2023-11-21 00:58:33 -0500

    finalized and worked

--- professorai/src/views/learnNew.js
+++ professorai/src/views/learnNew.js
@@ -1,13 +1,34 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../components/navbar';
 
 import "./styles/learnNew.css"; 
+import { sendExplore } from '../api/chatApi';
 
 function LearnNew(props) {
 
   {/* Generate a random fact for student with title: and content: */}
 
+  var knowledgeQuerry = [ {role: "user", content: "Generate a random fact for student with \"title: \" and \"content:\"" }]
+
     const [knowledge, setKnowledge] = useState({ title : "Valakushari" , content : "It is a nepali word to say that two people are talking ear to ear "})
+    
+    const fetchInfo = async() => {
+        
+      try
+      {
+      const response = await sendExplore(knowledgeQuerry);
+      setKnowledge(response);
+      }catch(error) {
+        console.error("Error", error);
+      }
+    }
+
+    useEffect( () => {
+
+      fetchInfo();
+
+    },[])
+
 
     return (
         <div className="learn-main">
@@ -17,7 +38,7 @@ function LearnNew(props) {
           <p className="learn-description">
             { knowledge.content }
           </p>
-            <button onClick={() => {} } className="submit-button">
+            <button onClick={ fetchInfo } className="submit-button">
               Generate New
             </button>
         </div>
